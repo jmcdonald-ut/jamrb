c8abe874ba6159c0da08a53191634e59d1aeffbb
Jonathon McDonald <jmcdonald.ut@gmail.com>
2016-02-06 11:28:22 -0700

    Prefer use of contracts over explicit parameter checking

--- jamrb-lex/source/utility.rkt
+++ jamrb-lex/source/utility.rkt
@@ -4,80 +4,77 @@
 ;;
 ;; By Jonathon McDonald
 #lang racket
-(provide tokenize tokenize-cons normalize-token-key
-         unget unget-with-position! watch-port-position! prepare-port-rewinder)
+
 
 ;;
 ;; Token Utilities
 ;;
 
-(define (tokenize line col key value)
-  (unless (number? line)
-    (raise-argument-error 'tokenize "number?" line))
-  (unless (number? col)
-    (raise-argument-error 'tokenize "number?" col))
-  (unless (symbol? key)
-    (raise-argument-error 'tokenize "symbol?" key))
-  (unless (string? value)
-    (raise-argument-error 'tokenize "string?" value))
+(provide
+ (contract-out
+  [tokenize (->* (exact-integer? exact-integer? symbol? string?) list?)]))
 
+(define (tokenize line col key value)
   `(Token ((,line ,col) ,(normalize-token-key key) ,value)))
 
-(define (tokenize-cons line col key value callback)
-  (unless (procedure? callback)
-    (raise-argument-error 'tokenize-cons "procedure?" callback))
 
-  (cons (tokenize line col key value) (callback)))
+(provide
+ (contract-out
+  [tokenize-cons
+   (-> exact-integer? exact-integer? symbol? string? procedure? pair?)]))
+
+(define (tokenize-cons line col key value callback)
+  (cons (tokenize line col key value)
+        (callback)))
+
+
+(provide (contract-out [normalize-token-key (-> symbol? symbol?)]))
 
 (define (normalize-token-key key)
-  (unless (symbol? key)
-    (raise-argument-error 'normalize-token-key "symbol?" key))
   (string->symbol (string-append "on_" (symbol->string key))))
+
 
 ;;
 ;; Port Utilities
 ;;
 
-(define (unget port [count 1])
-  (unless (port? port)
-    (raise-argument-error 'unget "port?" port))
-  (unless (exact-positive-integer? count)
-    (raise-argument-error 'unget "exact-positive-integer?" count))
+(provide (contract-out [unget (->* (port?) (exact-positive-integer?) port?)]))
 
+(define (unget port [count 1])
   (define pos (file-position port))
   (cond [(> pos (- 1 count)) (file-position port (- pos count))])
 
   port)
 
-(define (unget-with-position! port line col [count 1])
-  (unless (port? port)
-    (raise-argument-error 'unget-with-position! "port?" port))
-  (unless (exact-positive-integer? line)
-    (raise-argument-error 'unget-with-position! "exact-positive-integer?" line))
-  (unless (exact-integer? col)
-    (raise-argument-error 'unget-with-position! "exact-integer?" col))
-  (unless (exact-positive-integer? count)
-    (raise-argument-error
-     'unget-with-position! "exact-positive-integer?" count))
 
+(provide
+ (contract-out
+  [unget-with-position! (->* (port? exact-integer? exact-integer?)
+                             (exact-positive-integer?)
+                             port?)]))
+
+(define (unget-with-position! port line col [count 1])
   (if (> (file-position port) 0)
       (begin
         (set-port-next-location! port line col (file-position port))
         (unget port count))
       port))
 
+
+(provide
+ (contract-out
+  [watch-port-position! (-> port? (values exact-integer? exact-integer?))]))
+
 (define (watch-port-position! port)
   (port-count-lines! port)
   (let-values ([(line col _) (port-next-location port)])
     (values line col)))
 
-(define (prepare-port-rewinder port line col)
-  (unless (port? port)
-    (raise-argument-error 'preapre-port-rewinder "port?" port))
-  (unless (exact-positive-integer? line)
-    (raise-argument-error
-     'prepare-port-rewinder "exact-positive-integer?" line))
-  (unless (exact-integer? col)
-    (raise-argument-error 'prepare-port-rewinder "exact-integer?" col))
 
+(provide
+ (contract-out
+  [prepare-port-rewinder
+   (-> port? exact-positive-integer? exact-integer? procedure?)]))
+
+(define (prepare-port-rewinder port line col)
   (curry unget-with-position! port line col))
